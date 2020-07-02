eabaf930170c144fcb59e0125f9e95705dbe69b3
alexisteh <wenhui.teh23@gmail.com>
2020-07-01 21:34:40 -0400

    footer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
     def new 
         #go to form to log in with name, password
         @user = User.new 
+        @foot = "nofoot"
     end 
 
     def create 
